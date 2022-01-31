1482b83a8d0a4899e2c82b8951c67f6880772ab1
Roberson A <roberson_souza11@hotmail.com>
2022-01-31 20:20:22 -0300

    Startup feature

--- src/app/animations/startUpAnimation.js
+++ src/app/animations/startUpAnimation.js
@@ -1,6 +1,71 @@
+import anime from "animejs";
+
 const startUpAnimation = () => {
-    const startUpScreen = document.createElement('div')
-    const body = document.querySelector('body')
-}
+  const memoryAnimation = document.querySelector('.memory-animation');
+  
+  let timeline = anime.timeline({
+    easing: 'linear',
+    duration: 1000
+  })
+
+  timeline
+    .add({
+      targets: memoryAnimation,
+      complete: function() {
+        document.querySelector('.parent-memory-animation').style.opacity = 1
+      }
+    })
+    .add({
+      targets: memoryAnimation,
+      innerHTML: [640, 65536],
+      round:1,
+      easing: 'linear',
+      
+    })
+    .add({
+      targets: '.copyright-animation',
+      complete: function() {
+        document.querySelector('.copyright-animation').style.opacity = 1
+      }
+    })
+    .add({
+      targets:'.detecting-animation--1',
+      complete: function() {
+        document.querySelector('.detecting-animation--1').style.opacity = 1
+      }
+    })
+    .add({
+      targets:'.detecting-animation--2',
+      complete: function() {
+        document.querySelector('.detecting-animation--2').style.opacity = 1
+      }
+    })
+    .add({
+      targets:'.detecting-animation--3',
+      complete: function() {
+        document.querySelector('.detecting-animation--3').style.opacity = 1
+      }
+    }, '+= 2000')
+    .add({
+      targets:'.detecting-animation--4',
+      complete: function() {
+        document.querySelector('.detecting-animation--4').style.opacity = 1
+      }
+    }, '-= 500')
+    .add({
+      targets:'.startup-img-wrapper',
+      complete: function() {
+        document.querySelector('.startup-img-wrapper').style.zIndex = 20
+        document.querySelector('audio').play()
+      }
+    })
+    .add({
+      targets:['.startup-img-wrapper', '.startup-screen'],
+      complete: function() {
+        document.querySelector('.startup-img-wrapper').style.zIndex = -10
+        document.querySelector('.startup-screen').style.zIndex = -11
+      }
+    }, '+= 8000')
+};
 
 export default startUpAnimation;
